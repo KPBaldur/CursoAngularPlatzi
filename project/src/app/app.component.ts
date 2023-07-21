9e295e59eceeeeb19e50a2f16e3665b352d2630c
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-07-21 14:05:40 -0400

    Clase 7 - 8 -9- 10 - 11 -12 - 13 -14 -15

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,6 +17,46 @@ export class AppComponent {
     age: 29,
     avatar: 'https://i.pinimg.com/564x/5a/e2/da/5ae2dad5cc49cdbcc519a84d3a91b896.jpg'
   }
+
+  names: string[]= ['Juancho', 'Ceci', 'Isidora'];
+  newName = '';
+
+
+  movies: string[]= ['Avatar 2', 'Sonic', 'Mario Bross'];
+  newMovie = '';
+
+
+  products: Product[] = [
+    {
+      name : 'Pinky Pie',
+      price: 35000,
+      image: './assets/products/pinky pie.jpg',
+      category: 'toys'
+    },
+    {
+      name : 'Kriss Vector',
+      price: 800000,
+      image: './assets/products/kriss vector.jpg'
+    },
+    {
+      name : 'Dibujo',
+      price: 75000,
+      image: './assets/products/dibujo.jpg'
+    },
+    {
+      name : 'Car',
+      price: 1505000,
+      image: './assets/products/ford mustang.jpg'
+    },
+    {
+      name : 'watermelon',
+      price: 5000,
+      image: './assets/products/watermelon.jpg'
+    }
+
+  ]
+
+
 
   toogleButton() {
     this.btnDisable = !this.btnDisable;
@@ -36,5 +78,24 @@ export class AppComponent {
   changeName(event: Event){
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+/* AGREGAR UN NOMBRE AL ARRAY*/
+  addName(){
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number){
+    this.names.splice(index, 1);
+  }
+
+  addMovie(){
+    this.movies.push(this.newMovie);
+    this.newMovie = '';
+  }
+
+  deleteMovie(index: number){
+    this.movies.splice(index, 1);
   }
 }
